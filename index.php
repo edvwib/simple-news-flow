8318f7666cdf09b97b45e20e4200660c8d691853
edvwib <edde275@gmail.com>
2017-10-16 10:30:00 +0200

    add post template and moved stricttypes to top of file

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
+<?php declare(strict_types=1); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>SimpleNewsFlow</title>
 </head>
 <body>
 
+    <div class="post">
+        <h3 class="postTitle">Sample Title</h3>
+        <p class="postContent">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem error soluta consequatur ea veritatis beatae, neque, incidunt aspernatur quia debitis delectus labore eius illum libero quaerat nesciunt natus, architecto molestias.</p>
+        <p class="postAuthor">Sample Author</p>
+        <p class="postDate">2017-10-16</p>
+    </div>
+
+
+
+    <?php
+
+
+    ?>
+
+
 </body>
 </html>
-
-
-<?php
-
-declare(strict_types=1);
