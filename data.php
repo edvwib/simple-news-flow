ca154c0baff5572b476dc76b6a18cdcdfa7c8c2c
edvwib <edde275@gmail.com>
2017-10-16 14:47:04 +0200

    add arrays with data/content

--- data.php
+++ data.php
@@ -2,4 +2,16 @@
 
 declare(strict_types=1);
 
-$posts[] = "";
+
+
+$authors = [
+    'Edvin',
+    'Johan',
+    'Vincent',
+];
+$posts = [
+    0 => array('title' => 'Test0', 'date' => '2017-10-16', 'likes' => 13, 'content' => '0. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+    1 => array('title' => 'Test1', 'date' => '2017-09-16', 'likes' => 75, 'content' => '1. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+    2 => array('title' => 'Test2', 'date' => '2017-10-08', 'likes' => 28, 'content' => '2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+
+];
