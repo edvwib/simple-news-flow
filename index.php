195f48f5f401f5751d3031862d0bfaaf7fbff220
Edvin Wiberg <edde275@gmail.com>
2017-10-16 19:10:13 +0200

    link to js, change fetch of author

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/functions.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>SimpleNewsFlow</title>
+  <title>simple-news-flow</title>
   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
   <link href="style.css" rel="stylesheet">
 </head>
@@ -22,19 +22,25 @@ require_once __DIR__ . '/functions.php';
     <li class="navbarItem"><a href="">Contact Us</a></li>
 </nav>
 
+    <div id="posts">
+    <!-- Loop through the array with posts, and ouput the contents for each post
+        into one div.post element each, and get the authors name through authorId -->
     <?php foreach($posts as $author => $post): ?>
       <div class="post">
         <h3 class="postTitle"><?php echo $post['title']; ?></h3>
+        <hr>
         <p class="postContent"><?php echo $post['content']; ?></p>
+        <hr>
         <div class="postMeta">
-          <p class="postAuthor"><?php echo $authors[$author] ?></p>
+          <p class="postAuthor"><?php echo $authors[$post['authorId']]['user'] ?></p>
           <p class="postDate"><?php echo $post['date'] ?></p>
           <a class="postLikes">▲<?php echo $post['likes'] ?></a>
         </div>
       </div>
     <?php endforeach; ?>
-
+    </div>
 
 
 </body>
+<script src="script.js"></script>
 </html>
