1c5e12109580e5007fdbf3f56d52201b6a34431b
edvwib <edde275@gmail.com>
2017-10-17 11:02:57 +0200

    add sort function to sort posts by date

--- functions.php
+++ functions.php
@@ -1,8 +1,10 @@
 <?php
-
 declare(strict_types=1);
 
+function sortPostsByDate(array $a, array $b){
+    $format = 'Y-m-d'; //The format used in the $posts array
+    $aDate = date($format, strtotime($a['date'])); //Create a for $a & $b to compare
+    $bDate = date($format, strtotime($b['date']));
 
-function getPost(){
-
+    return $aDate > $bDate; //Sort descending
 }
