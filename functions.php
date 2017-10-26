4c8b8f6fc09f4268c0bd0b9d96bad884966f5e93
Edvin Wiberg <edde275@gmail.com>
2017-10-26 20:39:15 +0200

    move call to sort function

--- functions.php
+++ functions.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
 
-usort($posts, 'sortPostsByDate');
-
 function sortPostsByDate(array $a, array $b){
     $format = 'Y-m-d'; //The format used in the $posts array
     //var        create date object from format and date
